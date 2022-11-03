8868de3c15619dd326dbe1b84aec09f262941b42
Itochi <itochi@pm.me>
2022-11-02 21:49:13 -0300

    feat: add 5 games in a row, score and some cleaning

--- javascript.js
+++ javascript.js
@@ -1,99 +1,101 @@
-game();
+//Rock Paper Scissors Game from Odin Project
 
-function game(){
-
-    //score array
-    let score = [0, 0]
-
-for (let index = 0; index < 5; index++) {
 //string declaration to compare choices
 const possibilities = '012';
 
-//get user for it's choice
-const userChoiceString = prompt("Your choice").toLowerCase();
+//score array
+let score = [0, 0]
 
-//get app choice
-const appChoiceString = getComputerChoice();
 
-function getComputerChoice(){
-   const appChoiceArray = ['rock', 'paper', 'scissors'];
-   let randInt = Math.floor(Math.random() * 3);
-   return appChoiceArray[randInt];
-}
+game();
 
-//turn user choice to number
-const userChoice = choiceToNumber(userChoiceString);
+function game() {
 
-function choiceToNumber(choice){
+    for (let index = 0; index < 5; index++) {
 
-   switch (choice) {
-       case 'rock':
-           return '0';
-           break;
-       
-       case 'paper':
-           return '1';
-           break;
-   
-       case 'scissors':
-           return '2';
-           break;
+        //get user for it's choice
+        const userChoiceString = prompt("Your choice").toLowerCase();
 
-       default:
-           console.error('The choice must be either rock, paper or scissors');
-           break;
-   }
+        //get app choice
+        const appChoiceString = getComputerChoice();
+
+        //turn user's choice to number
+        const userChoice = choiceToNumber(userChoiceString);
+
+        //get result
+        const result = compareChoices(choiceToNumber(userChoiceString), choiceToNumber(appChoiceString));
+
+        //print result
+        printResult(result,userChoiceString,appChoiceString);
+
+    }
+
+    console.log("The Score is " + score[0] + " - " + score[1]);
 
 }
 
-//get result message
-const result = compareChoices(choiceToNumber(userChoiceString),choiceToNumber(appChoiceString));
+function choiceToNumber(choice) {
 
-//compare choices
+    switch (choice) {
+        case 'rock':
+            return '0';
+            break;
 
-function compareChoices(userChoice, appChoice){
+        case 'paper':
+            return '1';
+            break;
 
-if (possibilities.at(userChoice - 1) == appChoice){
-    score[0] += 1;
-   return 'won';
+        case 'scissors':
+            return '2';
+            break;
+
+        default:
+            console.error('The choice must be either rock, paper or scissors');
+            break;
+    }
+
 }
-else if (possibilities.at(userChoice - 2) == appChoice){
-    score[0] += 1;
-   return 'lose';
+
+function getComputerChoice() {
+    const appChoiceArray = ['rock', 'paper', 'scissors'];
+    let randInt = Math.floor(Math.random() * 3);
+    return appChoiceArray[randInt];
 }
 
-else{
-   return 'draw';
-}
+function compareChoices(userChoice, appChoice) {
+
+
+    if (possibilities.at(userChoice - 1) == appChoice) {
+        score[0] += 1;
+        return 'won';
+    }
+    else if (possibilities.at(userChoice - 2) == appChoice) {
+        score[0] += 1;
+        return 'lose';
+    }
+
+    else {
+        return 'draw';
+    }
 
 }
-
-//print result
-printResult(result);
 
 //print win/lose
-function printResult(result){
-   switch (result) {
-       case 'won':
-           console.log(`You Won! ${userChoiceString.charAt(0).toUpperCase() + userChoiceString.slice(1)} beats ${appChoiceString.charAt(0).toUpperCase() + appChoiceString.slice(1)}`);
-           break;
-   
-           case 'lose':
-               console.log(`You Lose! ${appChoiceString.charAt(0).toUpperCase() + appChoiceString.slice(1)} beats ${userChoiceString.charAt(0).toUpperCase() + userChoiceString.slice(1)}`)
-               break;
-   
-               case 'draw':
-           console.log('It\'s a Draw!')
-                   break;
-           
-       default:
-           break;
-   }
+function printResult(result, userChoiceString, appChoiceString) {
+    switch (result) {
+        case 'won':
+            console.log(`You Won! ${userChoiceString.charAt(0).toUpperCase() + userChoiceString.slice(1)} beats ${appChoiceString.charAt(0).toUpperCase() + appChoiceString.slice(1)}`);
+            break;
+
+        case 'lose':
+            console.log(`You Lose! ${appChoiceString.charAt(0).toUpperCase() + appChoiceString.slice(1)} beats ${userChoiceString.charAt(0).toUpperCase() + userChoiceString.slice(1)}`)
+            break;
+
+        case 'draw':
+            console.log('It\'s a Draw!')
+            break;
+
+        default:
+            break;
+    }
 }
-
-}
-
-console.log("The Score is " + score[0] + " - " + score[1]);
-
-}
-
